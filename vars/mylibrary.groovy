af5480b0b8ed3419d5084c5f1b53646d8e94cab3
NeverHoliday <hy.nasrun@gmail.com>
2021-05-22 21:19:02 +0700

    Fux

--- vars/mylibrary.groovy
+++ vars/mylibrary.groovy
@@ -1,11 +1,4 @@
 #!/usr/bin/env groovy
-// vars/YourStepName.groovy
-
-def call() {
-    // Do something here...
-}
-
-#!/usr/bin/env groovy
 
 def call( String name = 'human' ) {
     echo "Hello, ${name}."
